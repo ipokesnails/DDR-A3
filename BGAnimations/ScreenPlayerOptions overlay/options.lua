240bbb71da65ea7f93deae3eedd1f277ff236d0e
ipokesnails <56616525+ipokesnails@users.noreply.github.com>
2026-09-13 01:16:17 -0600

    Adjust size of favorite boss icon in options.lua

--- BGAnimations/ScreenPlayerOptions overlay/options.lua
+++ BGAnimations/ScreenPlayerOptions overlay/options.lua
@@ -329,7 +329,7 @@ local function MakeRow(rownames, idx)
 		-- Favorite boss icon
 		LoadActor(THEME:GetPathG("", "FavoriteIcon/Boss"))..{
 		    InitCommand=function(s)
-		        s:x(64):setsize(40,40):visible(false);
+		        s:x(64):setsize(30,30):visible(false);
 		    end,
 		
 		    OnCommand=function(s)
