c3cc6d76abf9cc5a81e2962f8906cee05ba5ab70
ipokesnails <56616525+ipokesnails@users.noreply.github.com>
2026-09-23 23:05:44 -0600

    Disable jacket loading for selected song
    Comment out jacket loading section for performance testing.

--- BGAnimations/ScreenSelectMusic overlay/Info/default.lua
+++ BGAnimations/ScreenSelectMusic overlay/Info/default.lua
@@ -130,23 +130,24 @@ return Def.ActorFrame{
 			-- end;
 		-- };
 	};
-																												
+														-- PERFORMANCE TESTING CHANGES
+														-- This entire section is being commented out to remove the top right jacket loading for the selected song
 	
-	Def.ActorFrame{
-		Name="Jacket";
-		LoadActor(Model().."jacket")..{ InitCommand=function(s) s:x(245):y(15) end, };
-		Def.Quad{ 
-			InitCommand=function(s) s:diffuse(Color.Black):diffusealpha(0.4):xy(244,17):setsize(152,152) end,
-		};
-		Def.Sprite{
-			InitCommand=function(s) s:x(244):y(17) end,
-			SetCommand=function(s)
-				local song = GAMESTATE:GetCurrentSong()
-				if song then
-					s:LoadFromCached("Jacket",GetJacketPath(song))
-				end
-				s:setsize(150,150)
-			end
-		};
-	};
-};
+--	Def.ActorFrame{
+--		Name="Jacket";
+--		LoadActor(Model().."jacket")..{ InitCommand=function(s) s:x(245):y(15) end, };
+--		Def.Quad{ 
+--			InitCommand=function(s) s:diffuse(Color.Black):diffusealpha(0.4):xy(244,17):setsize(152,152) end,
+--		};
+--		Def.Sprite{
+--			InitCommand=function(s) s:x(244):y(17) end,
+--			SetCommand=function(s)
+--				local song = GAMESTATE:GetCurrentSong()
+--				if song then
+--					s:LoadFromCached("Jacket",GetJacketPath(song))
+--				end
+--				s:setsize(150,150)
+--			end
+--		};
+--	};
+--};
